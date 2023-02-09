a9ce0b374220cd6713f2a01291b793391e9c5734
Augustoo22 <victoraugustodocarmo32@gmail.com>
2023-02-08 22:43:41 -0300

    Aula04

---
+++ Aula 04/js/aula.js
@@ -0,0 +1,17 @@
+/*window.alert("Ola mundo") //comentario de uma linha
+window.confirm("Voce deseja prosseguir")
+window.prompt("Digite seu nome: ")*/
+
+/*comentario
+de
+varias linhas*/ 
+
+/*
+var nome = window.prompt("Digite seu nome:")
+var idade = window.prompt("Digite sua idade:")
+window.alert("Bem-Vindo " + nome + " Você tem " + idade +" anos de idade") */
+
+var n1 = window.prompt("Digite numero 1:")
+var n2 = window.prompt("Digite numero 2:")
+var reultado = Number(n1) + Number(n2)
+window.alert("Resultado: " + reultado)
