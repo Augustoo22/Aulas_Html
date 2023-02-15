454daa3058ef892f9d83ff736ba6240bf09750e9
Augustoo22 <victoraugustodocarmo32@gmail.com>
2023-02-14 22:09:20 -0300

    Aula HTML

--- Aula 06/exerc2.js
+++ Aula 06/exerc2.js
@@ -3,17 +3,16 @@ var hora = data.getHours()
 var minutos = data.getMinutes()
 var dia = data.getDay()
 
-console.log(data)
+//Array armazena varias informaçoes
 
-console.log(dia)
-
-switch (hora) {
+switch (hora){
     case 12: 
     case 13: 
     case 14: 
     case 15: 
     case 16:
     case 17:
+        
         console.log("Boa tarde agora são: " + hora + ':' + minutos)
         break
     
@@ -23,6 +22,7 @@ switch (hora) {
     case 21: 
     case 22: 
     case 23:
+        
         console.log("Boa noite agora são: " + hora + ':' + minutos)   
         break
 
